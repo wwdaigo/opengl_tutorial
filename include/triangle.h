cce0fb17ae282742cd8bd4c1d7f94fb880b9e628
Daigo Matsuoka <daigo.matsuoka@navvis.com>
2019-06-04 11:23:05 +0200

    Describe the pipeline in methods

--- include/triangle.h
+++ include/triangle.h
@@ -6,15 +6,28 @@ public:
 	void draw();
 
 private:
+	const static GLfloat vertices[];
+
+	GLuint VBO, VAO;
 	GLuint vertexShader;
 	GLuint fragmentShader;
+	GLuint shaderProgram;
 
 	void setBgColor();
+
+	// Vertex array buffer
+	void setupVertexArrayBuffer();
 
 	// Shaders
 	void compileVertexShader();
 	void compileFragmentShader();
 	void debugShaderCompilation(GLuint shader, std::string tag);
+	void createShaderProgram();
+	void cleanupShader();
 
-	void drawTriangle();
+	// Rendering
+	void linkVertexAttributes();
+	void renderTriangle();
+
+	void runPipeline();
 };
--- src/triangle.cpp
+++ src/triangle.cpp
@@ -3,16 +3,33 @@
 #include <sstream>
 #include <string>
 
+const GLfloat Triangle::vertices[] = {
+	-0.5f,	-0.5f,	0.0f,
+	0.5f,	-0.5f,	0.0f,
+	0.0f,	0.5f,	0.0f
+};
+
 void Triangle::draw()
 {
 	setBgColor();
-	drawTriangle();
+	runPipeline();
 }
 
 void Triangle::setBgColor()
 {
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void Triangle::setupVertexArrayBuffer()
+{
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 }
 
 void Triangle::compileVertexShader()
@@ -46,49 +63,45 @@ void Triangle::debugShaderCompilation(GLuint shader, std::string tag)
 		throw std::runtime_error(ss.str());
 	}
 }
-
-void Triangle::drawTriangle()
+void Triangle::createShaderProgram()
 {
-	// 1- define the vertices of triangle, following x, y, z coordinates from -1 to 1 for each vertex.
-	GLfloat vertices[] = {
-		-0.5f,	-0.5f,	0.0f,
-		0.5f,	-0.5f,	0.0f,
-		0.0f,	0.5f,	0.0f
-	};
-
-	// 2 - Create the vertex buffer object (VBO) to submit to GPU the vertices to be rendered
-	// glGenBufers(n of buffers to be generated, reference to VBO)
-	GLuint VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	
-	GLuint VAO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	// Shaders process
-	compileVertexShader();
-	compileFragmentShader();
-		
-
-	// 5 - Create the shader program
-	GLuint shaderProgram;
 	shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
+}
 
+void Triangle::cleanupShader()
+{
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
+}
 
-	// 6 - Link the vertex attributes
+void linkVertexAttributes()
+{
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
+}
 
+void renderTriangle()
+{
 	glUseProgram(shaderProgram);
 	glBindVertexArray(VAO);
 	glDrawArrays(GL_TRIANGLES, 0 ,3);
 	glBindVertexArray(0);
+}
+
+void Triangle::runPipeline()
+{
+	// Vertex Array buffer / object
+	setupVertexArrayBuffer();	
+
+	// Shaders
+	compileVertexShader();
+	compileFragmentShader();
+	createShaderProgram();
+	cleanupShader();
+		
+	linkVertexAttributes();
+	renderTriangle();
 }
